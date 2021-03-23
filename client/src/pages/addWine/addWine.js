09129cb2e1bcb2d19b95c4a9e4c60048a855779d
JonM <jonmisner86@gmail.com>
2021-03-23 18:14:21 -0500

    completed addWine.js

--- client/src/pages/addWine/addWine.js
+++ client/src/pages/addWine/addWine.js
@@ -11,7 +11,6 @@ import "./addWine.css";
 import { Input } from "../../components/Form";
 // import { Link } from "react-router-dom";
 
-
 export default function AddWine() {
   const [wines, setWines] = useState([]);
   const [fullWineList, setFullWinesList] = useState([]);
@@ -60,7 +59,7 @@ export default function AddWine() {
         size: formObject.size,
         PPG: formObject.PPG,
         details: formObject.details,
-        date: new Date(Date.now())
+        date: new Date(Date.now()),
       })
         .then(() =>
           setFormObject({
@@ -72,127 +71,122 @@ export default function AddWine() {
         .catch((err) => console.log(err));
     }
   }
-  
+
   return (
     <div className="body">
       <Row className="header componenets">
-      <Col lg={8}></Col>
-          {wines.length > 0 && (
-            <Filters
-              fullWineList={fullWineList}
-              setWines={setWines}
-              wines={wines}
-              className="filters"
-            />
-          )}
+        <Col lg={8}></Col>
+        {wines.length > 0 && (
+          <Filters
+            fullWineList={fullWineList}
+            setWines={setWines}
+            wines={wines}
+            className="filters"
+          />
+        )}
       </Row>
       <Jumbotron fluid className="main">
         <Row>
           <Col lg={8}>
-            <Jumbotron fluid className="addWine"> 
-            <Form>
-            <Col className="formCol">
-              <Label className="labelColorChange">Winemaker</Label>
-              <Input
-                onChange={handleInputChange}
-                name="wine_name"
-                placeholder="Producer Name"
-              />
-              <Label className="labelColorChange">Label Name</Label>
-              <Input
-                onChange={handleInputChange}
-                name="full_name"
-                placeholder="Vineyard or label name"
-              />
-              <Label className="labelColorChange">Vintage</Label>
-              <Input
-                onChange={handleInputChange}
-                name="year"
-                placeholder="Vintage"
-              />
-              <Label>Color of Wine</Label>
-               <Input
-                onChange={handleInputChange}
-                name="color"
-                placeholder="Red, White, or Rose?"
-              />
-              <Label>Carbonation</Label>
-              <Input
-                className="inputColorChange"
-                onChange={handleInputChange}
-                name="carbonation"
-                placeholder="Still or Sparkling?"
-              />
-              <Label>Vatietal on Label</Label>
-              <Input
-               className="inputColorChange"
-                onChange={handleInputChange}
-                name="grape"
-                placeholder="Varietal or style of blend"
-              />
-              <Label>All Varietals Included</Label>
-              <Input
-               className="inputColorChange"
-                onChange={handleInputChange}
-                name="grape_detail"
-                placeholder="List of Varietals"
-              />
-              </Col>
-              <Col className="formCol">
-              <Label className="labelColorChange">Country</Label>
-              <Input
-                onChange={handleInputChange}
-                name="country"
-                placeholder="Country"
-              />
-              <Label className="labelColorChange">Region</Label>
-              <Input
-                onChange={handleInputChange}
-                name="region"
-                placeholder="Region"
-              />
-              <Label>Address</Label>
-              <Input
-                onChange={handleInputChange}
-                name="address"
-                placeholder="Address of Winery or Vineyard"
-              />
-              <Label>Glass Pour?</Label>
-                <Input
-                 className="inputColorChange"
-                onChange={handleInputChange}
-                name="size"
-                placeholder='"Glass" or "Bottle"'
-              />
-              <Label>Price Per Bottle</Label>
-              <Input
-               className="inputColorChange"
-                onChange={handleInputChange}
-                name="PPB"
-                placeholder="Price per bottle"
-              />
-              <Label>Price Per Glass</Label>
-              <Input
-               className="inputColorChange"
-                onChange={handleInputChange}
-                name="PPG"
-                placeholder="Price per glass (if available)"
-              />
-              <Label>Tasting Notes</Label>
-              <Input
-               className="inputColorChange"
-                onChange={handleInputChange}
-                name="details"
-                placeholder="Enter tasting notes here"
-              />
-               
-              <button
-						className="btn"
-						onClick={handleFormSubmit}
-						type="submit"
-					>Add Wine</button>
-            </Col>
-            </Form>
+            <Jumbotron fluid className="addWine">
+              <Form>
+                <Col className="formCol">
+                  <Label className="labelColorChange"> Winemaker </Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="wine_name"
+                    placeholder="Name of Producer"
+                  />
+                  <Label className="labelColorChange">Label Name</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="full_name"
+                    placeholder="Vineyard Name or Label Name"
+                  />
+                  <Label className="labelColorChange">Vintage</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="year"
+                    placeholder='Year Produced or "NV" for Non Vintage'
+                  />
+                  <Label>Color of Wine</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="color"
+                    placeholder="Red, White, or Rose?"
+                  />
+                  <Label>Carbonation</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="carbonation"
+                    placeholder="Still or Sparkling?"
+                  />
+                  <Label>Vatietal on Label</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="grape"
+                    placeholder='e.g. "Cabernet" or "Rhone Blend"'
+                  />
+                  <Label>All Varietals Included</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="grape_detail"
+                    placeholder="e.g. Cabernet Savignon, Merlot, Malbec"
+                  />
+                </Col>
+                <Col className="formCol">
+                  <Label className="labelColorChange">Country</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="country"
+                    placeholder="e.g. USA"
+                  />
+                  <Label className="labelColorChange">Region</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="region"
+                    placeholder="e.g. Napa Valley"
+                  />
+                  <Label>Address</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="address"
+                    placeholder="Address of Winery or Vineyard"
+                  />
+                  <Label>Glass Pour?</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="size"
+                    placeholder='"Glass" or "Bottle"'
+                  />
+                  <Label>Price Per Bottle</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="PPB"
+                    placeholder="Price Per Bottle"
+                  />
+                  <Label>Price Per Glass</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="PPG"
+                    placeholder="if not available by the glass, leave blank"
+                  />
+                  <Label>Tasting Notes</Label>
+                  <Input
+                    onChange={handleInputChange}
+                    name="details"
+                    placeholder="Enter tasting notes here"
+                  />
+
+                  <button
+                    className="btn"
+                    onClick={handleFormSubmit}
+                    type="submit"
+                  >
+                    Add Wine
+                  </button>
+                </Col>
+              </Form>
             </Jumbotron>
           </Col>
           <Col lg={4}>
@@ -201,18 +195,20 @@ export default function AddWine() {
                 <List>
                   {wines.map((wine) => (
                     <ListItem key={wine._id} wine={wine}>
-                    <Row>
-                      <Col lg={10}>
-                      <strong wine={wine}>
-                        {wine.wine_name} "{wine.full_name}"
-                      </strong>
-                      <div>
-                        {wine.year} {wine.grape} {wine.PPG} | {wine.PPB}
-                      </div>
-                      </Col>
-                      <Col lg={2}>
-                      <Info className="info" wine={wine}> </Info>
-                      </Col>
+                      <Row>
+                        <Col lg={10}>
+                          <strong wine={wine}>
+                            {wine.wine_name} "{wine.full_name}"
+                          </strong>
+                          <div>
+                            {wine.year} {wine.grape} {wine.PPG} | {wine.PPB}
+                          </div>
+                        </Col>
+                        <Col lg={2}>
+                          <Info className="info" wine={wine}>
+                            {" "}
+                          </Info>
+                        </Col>
                       </Row>
                     </ListItem>
                   ))}
@@ -227,4 +223,3 @@ export default function AddWine() {
     </div>
   );
 }
-
